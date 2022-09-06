a8910bd46b6d75421ce776dec7b41db953e25279
Kavitha-Yallavula <kavitha.yallavula@sunquestinfo.com>
2022-09-06 16:00:10 +0530

    updated maxLength prop

--- WebApp/src/constant/formFields.ts
+++ WebApp/src/constant/formFields.ts
@@ -168,14 +168,14 @@ const patientSearchFields = [
 
 const employerFields = [
   {
-    labelText: "Employer name",
+    labelText: "Employer Name",
     labelFor: "empName",
     id: "empName",
     name: "emp_name",
     type: "text",
     autoComplete: "emp_name",
     isRequired: true,
-    placeholder: "Employer name",
+    placeholder: "Employer Name",
   },
   {
     labelText: "Employer ID",
