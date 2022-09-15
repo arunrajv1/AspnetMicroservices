fa12411ba51e761848a76b4684e4a95c606aab78
AbbasAtrix94 <abbasatrix@gmail.com>
2022-09-15 12:12:30 +0530

    side panel changes

--- WebApp/src/components/common/ElementsUI/SidePanelComponent.tsx
+++ WebApp/src/components/common/ElementsUI/SidePanelComponent.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { Panel } from '@fluentui/react/lib/Panel';
+import React, { HTMLAttributes, useCallback, useEffect, useState } from 'react';
+import { IPanelProps, Panel } from '@fluentui/react/lib/Panel';
 import { useBoolean } from '@fluentui/react-hooks';
-import { Navigation24Regular, Power24Regular } from '@fluentui/react-icons';
-import { Dropdown } from '@fluentui/react';
+import { Dismiss24Regular, Navigation24Regular, Power24Regular } from '@fluentui/react-icons';
+import { Dropdown, IRenderFunction, Persona, PersonaSize } from '@fluentui/react';
 import { Button } from '@fluentui/react-components';
 import { loginRequest } from "../../../AuthConfig";
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
@@ -12,16 +12,21 @@ import cookies from 'js-cookie';
 
 const languageArray: any = languageOptions;
 const currentLanguageCode = cookies.get('i18next');
+let userArray: any;
 
 const SidePanelComponent = () => {
     const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] = useBoolean(false);
     const { instance, accounts, inProgress } = useMsal();
+    const [userName, setUserName] = useState("");
+
+    // console.log('use mal account: ', accounts[0].idTokenClaims, loggedInUser);
     // let currentLanguage = languageArray.find((x: any) => x.key == currentLanguageCode);
 
-    // useEffect(() => {
-    //     currentLanguage = languageArray.find((x: any) => x.key == currentLanguageCode);
-    //     console.log('current language', currentLanguage);
-    // }, [cookies.get('i18next')])
+    useEffect(() => {
+        userArray = accounts[0].idTokenClaims;
+        setUserName(userArray.family_name + ', ' + userArray.given_name);
+        console.log('use mal account: ', userArray);
+    })
 
     const handleLogout = () => {
         instance.logoutRedirect().catch(e => {
@@ -35,6 +40,22 @@ const SidePanelComponent = () => {
         dismissPanel();
     }
 
+    const handleHeaderContent: IRenderFunction<IPanelProps> = useCallback(
+
+        () => (
+            <div className='grid justify-start p-0' >
+                <Persona isOutOfOffice={true} text={userName} size={PersonaSize.size32} /><Dismiss24Regular />
+            </div>
+        ), [],
+
+    );
+
+    const handleHeaderContent2: any = () => {
+        <div className='grid justify-start p-0' >
+            <Persona isOutOfOffice={true} text={userName} size={PersonaSize.size32} />
+        </div>
+    }
+
     return (
         <div>
             <Button icon={<Navigation24Regular />} onClick={openPanel} />
@@ -43,17 +64,22 @@ const SidePanelComponent = () => {
                 onDismiss={dismissPanel}
                 isOpen={isOpen}
                 hasCloseButton={true}
-                headerText="Menu"
+                //headerTextProps={handleHeaderContent2}
+                //style={{maxWidth: "400px"}}
+            //onRenderNavigationContent={handleHeaderContent}
             >
+                <Persona isOutOfOffice={true} text={userName} size={PersonaSize.size32} />
+                <br></br>
                 <Dropdown
                     id="multiLanguage"
-                    label="Language"
+                    //label="Preffered Language"
                     options={languageArray}
                     defaultSelectedKey={languageArray.key}
                     selectedKey={languageArray.key}
                     onChange={handleLanguageChange}
                     style={{ marginBottom: "10px" }}
                     placeholder="Change Language"
+                    //disabled={languageArray.key == cookies.get('i18next')}
                 >
                 </Dropdown>
                 <Button icon={<Power24Regular />} id="btnLogout" type="button" onClick={handleLogout} >Logout </Button>
