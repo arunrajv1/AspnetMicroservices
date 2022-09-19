729eee652f2f5dff959ef87cae1ca2f812e24824
AbbasAtrix94 <abbasatrix@gmail.com>
2022-09-19 18:46:13 +0530

    fixed spinner full scree issue

--- WebApp/src/components/common/Loader/FullPageLoader.tsx
+++ WebApp/src/components/common/Loader/FullPageLoader.tsx
@@ -1,13 +1,16 @@
+import { Dialog, DialogSurface } from "@fluentui/react-components/unstable";
 import { Spinner } from "@fluentui/react-components";
 import { useState } from "react";
 
 const FullPageLoader = () => {
   return (
-    <div id='container' style={{position: "absolute"}} className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
-      <div className="text-center">
-        <Spinner size="huge" label="Loading..." />
+    <Dialog open={true}>
+      <div id='container' className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
+        <div className="text-center">
+          <Spinner size="huge" label="Loading..." />
+        </div>
       </div>
-    </div>
+    </Dialog>
   );
 };
 
